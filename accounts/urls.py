1fd1560c4da82cd5797e6f2e9769f2d54a172bc4
Michał Kawa <michal.kawa@hotmail.com>
2020-05-10 11:48:06 +0200

    Added github login through access token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 
-from accounts.views import FacebookLogin
+from accounts.views import FacebookLogin, GithubLogin
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
-    path('facebook/', FacebookLogin.as_view(), name='fb_login')
+    path('facebook/', FacebookLogin.as_view(), name='fb_login'),
+    path('github/', GithubLogin.as_view(), name='github_login')
 ]
