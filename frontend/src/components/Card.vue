7872e888277e94d218ec57b444d8b08f4dd9effd
Kamil Wozniak <jestem.kamil.wozniak@gmail.com>
2020-05-14 18:12:54 +0200

    feat: Create Card component

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card text-white mb-1 info-card my-card">
+    <div class="card-header">
+      <h4 class="text-white text-center text-capitalize title">{{title}}</h4>
+    </div>
+    <div class="card-body text-center">
+      <h2 class="font-weight-bold display-1">{{value}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Card",
+    props: {
+      title: {
+        type: String,
+        required: true,
+        default: "Title"
+      },
+      value: {
+        type: String,
+        required: true,
+        default: "Value"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.my-card {
+  background: linear-gradient(192.9deg, #00A896 -4.02%, rgba(255, 255, 255, 0) 119.96%), #02C39A;
+  box-shadow: 8px 8px 8px rgba(0, 0, 0, 0.25);
+  border-radius: 21px;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  max-width: 400px;
+}
+</style>
--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>Profile View</div>
+  <v-content>
+    <v-container fluid class="header-container">
+      <v-row>
+        <v-col cols="12" md="1"></v-col>
+        <v-col cols="12" md="3">
+          <Card title="My Balance" value="$821.47" />
+        </v-col>
+        <v-col cols="12" md="3">
+          <Card title="My Balance" value="$821.47" />
+        </v-col>
+        <v-col cols="12" md="3">
+          <Card title="My Balance" value="$821.47" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
+  import {mapGetters} from "vuex";
+  import Card from "../components/Card";
+
   export default {
-    name: "ProfileView"
+    name: "ProfileView",
+    components: {Card},
+    data() {
+      return {}
+    },
+    computed: {
+      ...mapGetters({
+        user: "getUser",
+        loading: "loading",
+        message: "getMessage"
+      })
+    }
   }
 </script>
 
