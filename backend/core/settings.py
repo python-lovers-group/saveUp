f1154e48d20ecde7d5acb38348ae3b3d174ec314
gb <gagsster@gmail.com>
2020-05-16 16:49:06 +0200

    Added browsable API template

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -89,7 +89,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,5 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/staticfiles/'
 
 django_heroku.settings(locals())
