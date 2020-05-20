f4a398cbc58fb9fd830c000dcac5fbc5b1087c65
Alicja <akrzeminska1519@gmail.com>
2020-05-20 16:20:06 +0200

    feat: Customized admin page - jet package

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 from app.models import Billing, Bill, Category
+from app.serializers import BillSerializer
 
-admin.site.register(Billing)
-admin.site.register(Bill)
+admin.site.site_header = "saveUp admin page"
+admin.site.unregister(Group)
+
+
+class BillAdmin(admin.ModelAdmin):
+    list_display = ('description', 'price', 'categories_name', 'billing_owner')
+    list_display_links = ('description',)
+    list_editable = ('price',)
+    list_filter = ('where', 'price')
+    search_fields = ('description',)
+
+    def billing_owner(self, obj):
+        return obj.billing.user
+
+    def categories_name(self, obj):
+        return BillSerializer(obj).data['categories']
+
+
+class InLineBill(admin.TabularInline):
+    model = Bill
+    extra = 1
+
+
+class BillingAdmin(admin.ModelAdmin):
+    inlines = (InLineBill,)
+    list_display = ('user', 'balance')
+
+    def balance(self, obj):
+        return sum([bill.price for bill in Bill.objects.filter(billing=obj)])
+
+
+admin.site.register(Bill, BillAdmin)
+admin.site.register(Billing, BillingAdmin)
 admin.site.register(Category)
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     # apps
     'user',
     'app',
+
 ]
 
 SITE_ID = 1
